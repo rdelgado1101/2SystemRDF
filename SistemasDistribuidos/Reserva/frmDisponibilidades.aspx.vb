b8075ba9a59a7fe3559e49c7d7d712f41aac63d9
jerikaupc <jerika.dedios@gmail.com>
2012-07-21 10:04:28 -0500

    FixBug.

--- SistemasDistribuidos/Reserva/frmDisponibilidades.aspx.vb
+++ SistemasDistribuidos/Reserva/frmDisponibilidades.aspx.vb
@@ -3,8 +3,6 @@ Partial Class frmDisponibilidades
     Inherits System.Web.UI.Page
 
     Public loBEDisponibilidad As List(Of SRConnector.BEAvailabilityResponse)
-
-
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Not Page.IsPostBack Then
             loBEDisponibilidad = Session("Vuelos")
